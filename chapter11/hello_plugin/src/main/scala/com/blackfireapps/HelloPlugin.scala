8ae34090922f8d7651c20156b6f3a5401b59bb75
darioalessandro <darioalessandrolencina@gmail.com>
2015-03-18 22:56:28 -0400

    Added shared sbt key

--- chapter11/hello_plugin/src/main/scala/com/blackfireapps/HelloPlugin.scala
+++ chapter11/hello_plugin/src/main/scala/com/blackfireapps/HelloPlugin.scala
@@ -6,13 +6,15 @@ package com.blackfireapps
  */
 
 import sbt._
+import sbt.Keys._
+import complete.DefaultParsers._
 
 object HelloPlugin extends sbt.AutoPlugin {
 
   lazy val helloTask = taskKey[Unit]("helloTask")
 
   override def projectSettings = Seq(
-    helloTask := println("Hello world")
+    helloTask := println("Hello " + (scalaSource in Compile).value.getAbsolutePath)
   )
 
 }
